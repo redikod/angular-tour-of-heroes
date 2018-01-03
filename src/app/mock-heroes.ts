2f050bd2cdffb1c808258ca312b729040f794e3e
Yousuf Zaman <redikod@gmail.com>
2018-01-04 02:42:31 +0600

    Angular 5 tutorial

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import {Hero} from './hero';
+
+export const HEROES : Hero[] = [
+    {id:11, name:'Yousuf Zaman'},
+    {id:12, name:'Saad Zaman'},
+    {id:13, name:'Shifat Rahman'},
+    {id:14, name:'Shamia'},
+    {id:15, name:'Zahid Islam'},
+    {id:16, name:'Shahriar Hossain'}
+];
