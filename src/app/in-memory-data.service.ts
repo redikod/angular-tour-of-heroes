450c1216535a44e347547fc9342108f3dd84f810
Yousuf Zaman <redikod@gmail.com>
2018-01-04 02:49:22 +0600

    data changes

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -3,16 +3,16 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-      { id: 11, name: 'Mr. Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 11, name: 'Yousuf Zaman' },
+      { id: 12, name: 'Haris-uz Zaman Saad' },
+      { id: 13, name: 'Shifat Rahaman' },
+      { id: 14, name: 'Shamiya' },
+      { id: 15, name: 'Jahidul Islam' },
+      { id: 16, name: 'Shahriar Hossain' },
+      { id: 17, name: 'Khalid Bin Habib' },
+      { id: 18, name: 'Techno Surf' },
+      { id: 19, name: 'Booking Spider' },
+      { id: 20, name: 'Cloud Nest' }
     ];
     return {heroes};
   }
